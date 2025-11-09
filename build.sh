cd0bca87a8a1d8e0db527a59d0a92327a92aac58
neil <nlongx@gmail.com>
2025-11-09 21:51:17 +0800

    代码重构

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 CUR_DIR=$(cd $(dirname $0);pwd)
 cd $CUR_DIR
+
 cargo build --target x86_64-unknown-linux-musl --release
+
+if [ $? -ne 0 ];then
+    echo "Build failed!"
+    exit 1
+fi
+
+if [ -f "rssh" ];then
+    rm rssh
+fi
+
 cp $CUR_DIR/target/x86_64-unknown-linux-musl/release/rssh rssh
 /bin/upx --best --lzma rssh
